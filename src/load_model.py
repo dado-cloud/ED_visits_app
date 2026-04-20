2046f3ada9d9ad88f0ab576193179deaf0664e17
dado-cloud <daad1mh@gmail.com>
2026-04-20 20:02:49 +0300

    Update load_model.py

--- src/load_model.py
+++ src/load_model.py
@@ -1,1 +1,20 @@
 
+import pickle
+import torch
+from pytorch_forecasting import TemporalFusionTransformer
+
+
+def load_tft_model(model_path):
+    model = TemporalFusionTransformer.load_from_checkpoint(
+        model_path,
+        map_location=torch.device("cpu")
+    )
+    model.eval()
+    torch.set_grad_enabled(False)
+    return model
+
+
+def load_training_dataset(path):
+    with open(path, "rb") as f:
+        training = pickle.load(f)
+    return training
